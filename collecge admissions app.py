6ec8eeaad51eaeee5beb1a765b4f4407036dee7c
“javascriptseo” <seofreelance2021@outlook.com>
2023-09-22 22:06:54 -0700

    first commit

--- collecge admissions app.py
+++ collecge admissions app.py
@@ -1,15 +1,18 @@
-# here I am gettinG user inputs
+# here I am getting the user inputs for test score and class rank
 testScoreString = input("Type your score here")
 classRankString = input("type class rank here")
 
-# then I am converting the string to integer with int
+# then I am converting the string to integer with the int function
 testScore = int(testScoreString)
 classRank = int(classRankString)
 
 # The rest of the code checks the admissions acceptance
+# if the score is greater than or equal to 90, and the classrank is greater or equal to 25
 if testScore >= 90:
     if classRank >= 25:
+        # then print accept
         print("Accept")
+    #     if not print reject
     else:
         print("Reject")
 else:
